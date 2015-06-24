aff66d2cbc7bd690f81d4560a619f74f57ecce16
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-06-24 11:11:51 +0900

    gitbucket のログが正常に出力されるように修正

--- gitbucket/startup.sh
+++ gitbucket/startup.sh
@@ -3,6 +3,6 @@
 chown -R gitbucket:gitbucket /var/log/gitbucket
 chown -R gitbucket:gitbucket /opt/gitbucket
 
-sudo -u gitbucket sh -c "/usr/bin/java -jar /opt/gitbucket/gitbucket.war --port=52201 --gitbucket.home=/opt/gitbucket/data >> /var/log/gitbucket/gitbucket.log"
+sudo -u gitbucket sh -c "/usr/bin/java -jar /opt/gitbucket/gitbucket.war --port=52201 --gitbucket.home=/opt/gitbucket/data >> /var/log/gitbucket/gitbucket.log 2>&1"
 
 exit 0
