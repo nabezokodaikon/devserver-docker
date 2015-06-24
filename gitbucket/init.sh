d53b0523a3436018ae896431cdc66102d28cc5f5
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-06-24 19:55:29 +0900

    スクリプト名を変更

--- gitbucket/init.sh
+++ gitbucket/init.sh
@@ -6,13 +6,11 @@ chown -R gitbucket:gitbucket /opt/gitbucket
 sudo -u gitbucket sh -c "/usr/bin/java -jar /opt/gitbucket/gitbucket.war --port=52201 --gitbucket.home=/opt/gitbucket/data >> /var/log/gitbucket/gitbucket.log 2>&1" &
 
 term() {
-    echo samplelog >> /var/log/gitbucket/sample.log
+    echo 'call kill.' >> /var/log/gitbucket/test.log
 }
 
-trap "term; exit" SIGTERM
+trap "term; exit 0" SIGKILL
 
 while true; do
     sleep 1
 done
-
-exit 0
