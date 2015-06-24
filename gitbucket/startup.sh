5d9ff937d0ce407b42c8ee00db04610b346170f2
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-06-24 10:58:29 +0900

    gitbucket のログ出力対応

--- gitbucket/startup.sh
+++ gitbucket/startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+chown -R gitbucket:gitbucket /var/log/gitbucket
 chown -R gitbucket:gitbucket /opt/gitbucket
-sudo -u gitbucket sh -c "/usr/bin/java -jar /opt/gitbucket/gitbucket.war --port=52201 --gitbucket.home=/opt/gitbucket/data"
+
+sudo -u gitbucket sh -c "/usr/bin/java -jar /opt/gitbucket/gitbucket.war --port=52201 --gitbucket.home=/opt/gitbucket/data >> /var/log/gitbucket/gitbucket.log"
 
 exit 0
